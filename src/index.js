4d7195fb38d2f54b60465f555bd9d8e7504aa788
Davide Cariani <davide.cariani@letgo.com>
2016-09-01 17:17:51 +0200

    export general Arrow component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export Slider from './Slider.js';
 export Slides from './Slides.js';
-export { PrevArrow, NextArrow } from './Arrows.js';
+export { PrevArrow, NextArrow, Arrow } from './Arrows.js';
 export Dots from './Dots.js';
