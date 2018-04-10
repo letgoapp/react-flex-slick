fdec4ced77feac0f42dbee676bc349a02c6a0aba
Dani <dani.grande@letgo.com>
2018-04-10 15:25:41 +0200

    adding prop-types module

--- src/Arrows.js
+++ src/Arrows.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import prefixAll from 'inline-style-prefixer/static';
 
 function isActive(currentSlide, slideCount, infinite, next, prev) {
